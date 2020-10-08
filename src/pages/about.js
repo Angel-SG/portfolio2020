c36340cd5f6b3ae11555f925d28bb571a626b701
Angel <angel@Angels-iMac.local>
2020-10-08 14:32:46 +0100

    updated content

--- src/pages/about.js
+++ src/pages/about.js
@@ -51,37 +51,22 @@ const AboutPage = () => {
               <div className="about-main-copy">
                 <div className="copy-section">
                   <h2>Open to opportunities</h2>
-                  <p>I'm currently available for freelance and contract work. I'll be happy to discuss your requirements and to provide a cost and time estimates for your project.</p>
+                  <p>I'm currently available for freelance and contract work. I'll be happy to discuss your requirements and to provide cost and time estimates for your project.</p>
                 </div>
                 <div className="copy-section">
                   <h2>Skills & Expertise</h2>
                   <h3>Front-end Development</h3>
-                  <p>I'm specialised in bringing designs and mock-ups to fully functioning web pages by using technologies like HTML, CSS and javaScript. I also have experience coding with React.js and building fast and reliable UI's. When it comes to CMS's I have worked with Drupal and Contentful. </p>
+                  <p>Specialized in bringing designs and mock-ups to fully functioning web pages by using technologies like HTML, CSS, and javaScript. I also have experience coding with React.js and building fast and reliable UI. Understanding of how to work with CMS like WordPress or Contentful.</p>
                   <h3>Responsive Design</h3>
-                  <p>Responsive web designs which will be multi-device compatible to give users high-quality browsing experience and user journey.</p>
+                  <p>Responsive web designs that will be multi-device compatible to give users high-quality browsing experience and user journey.</p>
                   <h3>Site optimization</h3>
                   <p>With the help of tools like Google Lighthouse and GTMetrics, I can have an insight into how websites perform on the web. Then by tacking this information into consideration, I apply the required changes to achieve the best possible performance.</p>
                   <h3>Marketing</h3>
-                  <p>I have participated in several marketing campaigns by coding both HTML email templates and landing pages. I'm familiar with email services and frameworks like Foundation for emails, Mailchimp, Pure360 and testing tools like EOA or Litmus. </p>
+                  <p>I have participated in several marketing campaigns by coding both HTML email templates and landing pages. I'm familiar with email services and frameworks like Foundation for emails, Mailchimp, Pure360, and testing tools like EOA or Litmus.</p>
                   <h3>Cross-browser compatibility </h3>
-                  <p>All developed web pages are completely tested to ensure compatibility with different web browsers including Firefox, Google Chrome, Safari and different versions of Internet Explorer.</p>
+                  <p>All developed web pages are tested to ensure compatibility with different web browsers including Firefox, Google Chrome, Safari, and several versions of Internet Explorer.</p>
                   
                 </div>
-
-                {/* <div className="copy-section">
-                  <h2>Work history</h2>
-                  <h3>The Eventa Group</h3>
-                  <p>I'm specialised in bringing designs and mock-ups to fully functioning web pages by using technologies like HTML, CSS and javaScript. I also have experience coding with React.js and building fast and reliable UI's. When it comes to CMS's I have worked with Drupal and Contentful. </p>
-                  <h3>Responsive Design</h3>
-                  <p>Responsive web designs which will be multi-device compatible to give users high-quality browsing experience and user journey.</p>
-                  <h3>Site optimization</h3>
-                  <p>With the help of tools like Google Lighthouse and GTMetrics, I can have an insight into how websites perform on the web. Then by tacking this information into consideration, I apply the required changes to achieve the best possible performance.</p>
-                  <h3>Marketing</h3>
-                  <p>I have participated in several marketing campaigns by coding both HTML email templates and landing pages. I'm familiar with email services and frameworks like Foundation for emails, Mailchimp, Pure360 and testing tools like EOA or Litmus. </p>
-                  <h3>Cross-browser compatibility </h3>
-                  <p>All developed web pages are completely tested to ensure compatibility with different web browsers including Firefox, Google Chrome, Safari and different versions of Internet Explorer.</p>
-                  
-                </div> */}
             </div>
             </Col>
           </Row>
